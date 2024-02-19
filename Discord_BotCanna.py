e3cf3e00309dff86e9d912320372190308f1f355
Raul Bernal <bernal.raul@gmail.com>
2024-02-19 15:58:39 +0100

    Duplicate for mainnet command and customize it

--- Discord_BotCanna.py
+++ Discord_BotCanna.py
@@ -38,7 +38,7 @@ async def on_message(message):
 
 # IS USED FOR THE FAUCET FUNCTION. USER SENDS THEIR ADDRESS IT COINS WILL BE DISTRIBUTED AUTOMATICALLY.
 # WILL ONLY WORK IF THE USER HAS THE ROLE INVITATIONAL TESTER, THIS WAY WE CAN PREVENT REGULAR USERS TO USE THIS COMMAND.
-@client.command(name='claim', hidden=True)
+@client.command(name='testnet', hidden=True)
 #@commands.has_role('invitational tester')
 async def claim(ctx, address=None):
     # If user doesn't send an address with the command, the bot will tell the user he/she has to include it
@@ -96,6 +96,70 @@ async def claim(ctx, address=None):
                 tx_hash = info['result']
                 # Bot will send a message together with the txhash of the distribution.
                 await ctx.message.reply(f'Distribution initiated, you will receive your test coins shortly. \nYour tx hash is https://testnet.ping.pub/bitcanna/tx/{str(tx_hash)}')
+            else:
+                print ('Data gathering not successful')
+                await ctx.message.reply('Data gathering not successful. Unfortunately something went wrong during your claim. Please contact one of our Admins for support')            
+    else:
+        await ctx.message.reply('Unfortunately something went wrong during your claim. Please contact one of our Admins for support')
+
+@client.command(name='mainnet', hidden=True)
+#@commands.has_role('invitational tester')
+async def claim(ctx, address=None):
+    # If user doesn't send an address with the command, the bot will tell the user he/she has to include it
+    if address == None:
+        await ctx.message.reply('Address is missing, include your address in the command like this: !mainnet ADDRESS')
+        return
+    # This checks if the address is valid
+    urlcheck = 'https://lcd.bitcanna.io/cosmos/auth/v1beta1/accounts/'+address
+    try:
+        responsecheck = requests.get(urlcheck,headers={'Accept': 'application/json'},)
+    except:
+        await ctx.message.reply('Something went wrong with API, connection error')
+
+    json_response = responsecheck.json()
+    # If the response contains error code 3 this means the address is invalid: malformed bech32
+    try:
+        code = str(json_response['code'])
+    except:
+        code = None
+
+    if code == '3':
+        await ctx.message.reply(f'{address} is a not valid address, make sure you have copied it properly.')
+        return
+    # If the response contains error code 5 this means the address is valid and not funded yet. If it doesnt have the error code in the response, the account is valid and already funded.
+    # Still, they cant claim twice because of the ClaimedList, so should be ok.
+    elif code == '5' or code == None:
+        # Checks if user is already in the ClaimedList, if yes then it will tell the user.
+        with open('ClaimedList_mainnet.txt', 'r') as f:
+            # Sends a log message to bot-test channel. This enables admins to quickly see who is claiming.
+            channel = client.get_channel(807011691401183234)
+            await channel.send(f'{ctx.author} / {ctx.author.id} has tried to claim')
+            print(str(ctx.author) + ' / ' + str(ctx.author.id) + ' has tried to claim')
+            ClaimedUsers = [line.strip() for line in f]
+            if str(ctx.author.id) in ClaimedUsers:
+                await ctx.message.reply('It looks like you have claimed already! This is not allowed ;) \n' 
+                'If this is a mistake, please contact one of our Admins.')
+                return
+        # Admins will not be added to the ClaimedList, so admins can claim infinitely
+        AdminList = ['398132337965269022','805724606324801557','804687093266907186','412915923247169537','431473976146001940','371035381853847552','293167458603237376']
+        if str(ctx.author.id) in AdminList :
+            pass
+        else:
+            # If the user is not yet in the Claimedlist, the user will be added of course. So next time he will be unable to claim again.
+            with open('ClaimedList_mainnet.txt', 'a') as f:
+                f.write('\n' + str(ctx.author.id))
+        urlclaim = 'http://localhost:9000/faucet/claim/'+address
+        try:
+            # This calls the script of Atmon3r together with the user ADDRESS
+            responseclaim = requests.request("GET", urlclaim,headers={'Accept': 'application/json'},)
+        except:
+            await ctx.message.reply('Something went wrong with the faucet, connection error')
+        else:
+            if responseclaim.status_code == 200:
+                info = json.loads(responseclaim.text)
+                tx_hash = info['result']
+                # Bot will send a message together with the txhash of the distribution.
+                await ctx.message.reply(f'Distribution initiated, you will receive your MainNET coins shortly. \nYour tx hash is https://explorer.bitcanna.io/transactions/{str(tx_hash)}')
             else:
                 print ('Data gathering not successful')
                 await ctx.message.reply('Data gathering not successful. Unfortunately something went wrong during your claim. Please contact one of our Admins for support')            
